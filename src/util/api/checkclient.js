02fc1cc05ef7ae5a06d8f8690ba5f871ede89c14
andreFnicacio <andrenicaciodev@gmail.com>
2024-07-05 10:05:07 -0300

    Fixed API Routes v2

--- src/util/api/checkclient.js
+++ src/util/api/checkclient.js
@@ -7,33 +7,44 @@ const updateClient = require("../api/updateClient");
 async function checkClientExists(numberId) {
     console.log("Iniciando Checagem");
     try {
-        const sessionKey = `session_${numberId}`;                
-        let client = await redis.getUserState(sessionKey);
-
-        if (!client) {
-            console.log("Client Redis not found");            
-            const response = await axios.get(`https://api-wpp-production-d36f.up.railway.app/client/check?numberid=${numberId}`);
-            if (response.data.exists) {
-                client = response.data.exists;
-                client.deadline = 86400;                
-            } else {
-                return false;
-            }
-        }
-
-        if (!client.token) {
-            console.log("Sessao nao foi criada");            
-            return client;            
-        }else{
-            const check = await isSessionActive(client);
-            if (check) {
-                client.deadline = 86400;                
-                return client;
-            } else {
-                new_client = await handleInactiveSession(client, sessionKey);
-                return new_client;
-            }
-        }        
+    //    const sessionKey = `session_${numberId}`;                
+    //    let client = await redis.getUserState(sessionKey);
+//
+    //    if (!client) {
+    //        console.log("Client Redis not found");            
+    //        const response = await axios.get(`https://api-wpp-production-d36f.up.railway.app/client/check?numberid=${numberId}`);
+    //        if (response.data.exists) {
+    //            client = response.data.exists;
+    //            client.deadline = 86400;                
+    //        } else {
+    //            return false;
+    //        }
+    //    }
+//
+    //    if (!client.token) {
+    //        console.log("Sessao nao foi criada");            
+    //        return client;            
+    //    }else{
+    //        const check = await isSessionActive(client);
+    //        if (check) {
+    //            client.deadline = 86400;                
+    //            return client;
+    //        } else {
+    //            new_client = await handleInactiveSession(client, sessionKey);
+    //            return new_client;
+    //        }
+    //    }   
+    return {
+        name: "Vinicius",
+        email: "vinicius@iftm.edu.com.br",
+        phone: numberId,
+        unique_key: "unique_key",
+        id_session: `session_${numberId}`,
+        id_phone: number,
+        session_status: false,
+        flow_roadmap: "signup_flow",
+        step_flow: "start" 
+    }
     } catch (error) {
         console.error('Erro ao verificar cliente: ', error);
         return false;
