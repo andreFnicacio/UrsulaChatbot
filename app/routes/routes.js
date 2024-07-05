c49f07b273b5d6d783f969e3a17ffde3b0955e78
andreFnicacio <andrenicaciodev@gmail.com>
2024-07-05 12:32:44 -0300

    Fixed API Routes v3.5

--- app/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var expres = require("express");
-var router = expres.Router();
-var whatsAppController = require("../controllers/whatsappControllers");
-router.get("/", whatsAppController.VerifyToken).post("/status", whatsAppController.StatusSession).post("/", whatsAppController.ReceivedMessage);
-module.exports = router;
